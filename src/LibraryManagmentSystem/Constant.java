8fb22fba87bd93b09ba0381faa776df25591b7bd
ShwetaV17 <vermasweta408@gmail.com>
2023-07-31 22:45:26 +0530

    Updated user input

--- src/LibraryManagmentSystem/Constant.java
+++ src/LibraryManagmentSystem/Constant.java
@@ -2,6 +2,6 @@ package LibraryManagmentSystem;
 
 public class Constant
 {
-    public static final String USER_INPUT = "Enter the value :";
+    public static final String USER_INPUT = "Enter the user value :";
 
 }
